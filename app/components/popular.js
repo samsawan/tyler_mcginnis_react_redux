554bc2ffeffa1f026497df6f12bcc7bf91de42fb
Sam Sawan <ssawan@onshift.com>
2018-02-07 16:08:28 -0500

    selected language is now a thing

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,17 +1,38 @@
 var React = require('react');
 
 class Popular extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedLanguage: 'All'
+		};
+
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+
+	updateLanguage(newLanguage) {
+		this.setState(function() {
+			return {
+				selectedLanguage: newLanguage
+			}
+		});
+	}
+
 	render() {
 		var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 		return (
 			<ul className='languages'>
+			<p>Selected Language: {this.state.selectedLanguage}</p>
 			{languages.map(function(language) {
 				return(
-					<li>
+					<li
+						key={language}
+						onClick={this.updateLanguage.bind(null, language)}>
 						{language}
 					</li>
 				)
-			})}
+			}, this)}
 			</ul>
 		)
 	}
