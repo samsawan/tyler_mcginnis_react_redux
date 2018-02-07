fc4cdb4212d946bb44d9f87604ced772a5cdb12e
Sam Sawan <ssawan@onshift.com>
2018-02-07 12:07:11 -0500

    add webpack create script
    fix typo

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component(
+class App extends React.Component {
 	// whatever the render method is the specific ui of the component
 	render() {
 		return(
@@ -11,7 +11,7 @@ class App extends React.Component(
 			<div>Hello World!</div>
 		)
 	}
-)
+}
 
 ReactDOM.render(
 	// what to render
