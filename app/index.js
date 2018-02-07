0c510054b01bebbcc7c21816e6d3ca0a0f5eb5f3
Sam Sawan <ssawan@onshift.com>
2018-02-07 11:44:05 -0500

    adding skeletal js

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+
+class App extends React.Component(
+	// whatever the render method is the specific ui of the component
+	render() {
+		return(
+			// this is JSX, not valid JS syntax
+			// this is why we need babel 
+			<div>Hello World!</div>
+		)
+	}
+)
+
+ReactDOM.render(
+	// what to render
+	<App />,
+	// where to render
+	document.getElementById('app')
+);
