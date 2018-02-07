7c8f5d33e7fc948cefe8c55f69b102cb0c852d0c
Sam Sawan <ssawan@onshift.com>
2018-02-07 15:40:30 -0500

    create components folder
    break out app into that

--- app/index.js
+++ app/index.js
@@ -2,20 +2,9 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-	// whatever the render method is the specific ui of the component
-	render() {
-		return(
-			// this is JSX, not valid JS syntax
-			// this is why we need babel 
-			<div>Hello World!</div>
-		)
-	}
-}
+var App = require('./components/App')
 
 ReactDOM.render(
-	// what to render
 	<App />,
-	// where to render
 	document.getElementById('app')
 );
