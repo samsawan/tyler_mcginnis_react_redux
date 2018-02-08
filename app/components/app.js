a5d5b0c85c8963ea9a7208d949eaafeac48aa817
Sam Sawan <ssawan@onshift.com>
2018-02-08 14:10:34 -0500

    paths working with switch

--- app/components/app.js
+++ app/components/app.js
@@ -2,9 +2,12 @@ var React = require('react');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 var Popular = require('./popular');
 var Nav = require('./nav');
+var Home = require('./home');
+var Battle = require('./battle');
 
 
 class App extends React.Component {
@@ -13,7 +16,14 @@ class App extends React.Component {
 			<Router>
 				<div className='container'>
 					<Nav />
-					<Route path='/popular' component={Popular} />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/popular' component={Popular} />
+						<Route exact path='/battle' component={Battle} />
+						<Route render={function() {
+							return <p>Not Found</p>
+						}} />
+					</Switch>
 				</div>
 			</Router>
 		)
