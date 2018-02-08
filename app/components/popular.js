7c2bbefc0a0a8a7c496a28fe0382f107d853311d
Sam Sawan <ssawan@onshift.com>
2018-02-08 11:00:54 -0500

    stateless function components ftw

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,26 +1,24 @@
 var React = require('react');
 
-class SelectLanguage extends React.Component {
-	render() {
-		var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+function SelectLanguage(props) {
+	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
-		return (
-				<ul className='languages'>
-				{languages.map(function(language) {
-					return (
-							<li
-								style={language === this.props.selectedLanguage ? { color: '#d0021b'} : null}
-								key={language}
-								onClick={this.props.updateLanguage.bind(null, language)}
-							>
-								{language}
-							</li>
-						)
-					}, this)
-				}
-			</ul>
-		)
-	}
+	return (
+		<ul className='languages'>
+			{languages.map(function(language) {
+				return (
+						<li
+							style={language === props.selectedLanguage ? { color: '#d0021b'} : null}
+							key={language}
+							onClick={props.updateLanguage.bind(null, language)}
+						>
+							{language}
+						</li>
+					)
+				})
+			}
+		</ul>
+	)
 }
 
 class Popular extends React.Component {
