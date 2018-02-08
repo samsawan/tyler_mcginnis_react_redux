59082605ebeb733de73cd966b2e7f3d18bf75fe4
Sam Sawan <ssawan@onshift.com>
2018-02-08 13:42:53 -0500

    make keys unique if dup repo names

--- app/components/popular.js
+++ app/components/popular.js
@@ -27,7 +27,7 @@ function RepoGrid(props) {
 		<ul className='popular-list'>
 			{props.repos.map(function(repo, index) {
 				return (
-					<li key={repo.name} className='popular-item'>
+					<li key={repo.name + index} className='popular-item'>
 						<div className='popular-rank'>#{index + 1}</div>
 						<ul className='space-list-items'>
 							<li>
